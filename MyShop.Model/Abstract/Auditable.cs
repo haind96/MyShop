d5654f993e1a61495a308ed04f9abc341c43eae5
haind96 <nguyendinhhai196@gmail.com>
2023-01-17 17:19:53 +0700

    update

---
+++ MyShop.Model/Abstract/Auditable.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace TeduShop.Model.Abstract
+{
+    public abstract class Auditable : IAuditable
+    {
+        public DateTime? CreatedDate { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public string CreatedBy { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public DateTime? UpdatedDate { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public string UpdatedBy { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public string MetaKeyword { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public string MetaDescription { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public bool Status { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+    }
+}
