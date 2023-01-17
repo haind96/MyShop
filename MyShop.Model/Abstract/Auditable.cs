18e090aa3e8b18c3cd0f74c07b19d313d3e40b32
haind96 <nguyendinhhai196@gmail.com>
2023-01-17 15:44:11 +0700

    update

---
+++ MyShop.Model/Abstract/Auditable.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MyShop.Model.Abstract
+{
+    public abstract class Auditable : IAuditable
+    {
+        public DateTime? CreatedDate { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        [MaxLength(256)]
+        public string CreatedBy { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        public DateTime? UpdateDate { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        [MaxLength(256)]
+        public string UpdateBy { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        [MaxLength(256)]
+        public string MetaKeyword { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        [MaxLength(256)]
+        public string MetaDescription { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        public bool Status { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+    }
+}
---
+++ MyShop.Model/Models/Slide.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MyShop.Model.Models
+{
+    [Table("Slides")]
+    public class Slide
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int ID { get; set; }
+
+        [Required]
+        [MaxLength(256)]
+        public string Name { get; set; }
+
+        [MaxLength(256)]
+        public string Descriptionn { get; set; }
+
+        [MaxLength(256)]
+        public string Image { get; set; }
+
+        [MaxLength(256)]
+        public string Url { get; set; }
+
+        public int? DisplayOrder { get; set; }
+
+        public bool Status { get; set; }
+
+        public string Content { get; set; }
+    }
+}
